898c3bbeb32c0109ce96e7023880b31ca5c786d1
AndrejTesten <andrej.testen33@gmail.com>
2024-04-29 02:32:53 +0200

    Basic logic of a game created

---
+++ jsdpquiz/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+import Question from "./components/Question.js";
+function App() {
+  return <Question />;
+}
+
+export default App;
---
+++ jsdpquiz/src/components/Question.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { quiz } from "../utils/questions.js";
+const Quiz = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+  const onClickNext = () => {
+    setSelectedAnswerIndex(null);
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    );
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+    }
+  };
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log("true");
+    } else {
+      setSelectedAnswer(false);
+      console.log("false");
+    }
+  };
+  //desctructuring
+  const { questions } = quiz;
+  const { correctAnswer } = questions[activeQuestion];
+
+  const { question, choices } = questions[activeQuestion];
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
+
+  return (
+    <div className="quiz">
+      {" "}
+      {!showResult ? (
+        <>
+          <div className="questionsGroup">
+            <span className="active-question-no">
+              {addLeadingZero(activeQuestion + 1)}
+            </span>
+            <span className="total-question">
+              /{addLeadingZero(questions.length)}
+            </span>
+            <h2 className="question">{questions[activeQuestion].question}</h2>
+          </div>
+          <ul className="answers">
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => onAnswerSelected(answer, index)}
+                key={answer}
+                className={
+                  selectedAnswerIndex === index ? "selected-answer" : null
+                }
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <button onClick={onClickNext} disabled={selectedAnswerIndex === null}>
+            {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
+          </button>
+        </>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Question: <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score:<span> {result.score}</span>
+          </p>
+          <p>
+            Correct Answers:<span> {result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers:<span> {result.wrongAnswers}</span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
